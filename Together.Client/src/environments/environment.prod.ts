2da32af6bb570ba1ad34b3234488c27ce182325e
toilavinh <hoangdvinh68@gmail.com>
2024-07-04 22:42:24 +0700

    fix nginx.conf & angular build budgets

--- Together.Client/src/environments/environment.prod.ts
+++ Together.Client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  host: 'http://14.225.211.153',
+  host: 'http://togethernet.asia',
   googleClientId:
     '82706818556-1ouk2iad6jsf0l38f9vjmqpkkb59nf97.apps.googleusercontent.com',
 };
